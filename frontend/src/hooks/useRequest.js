4fa21c7063cac80c12f15d9da3129c078a57549f
TUTOR03 <artemss20030906@gmail.com>
2020-11-16 09:07:47 +0500

    add_request_hook

--- frontend/src/hooks/useRequest.js
+++ frontend/src/hooks/useRequest.js
@@ -5,7 +5,28 @@ export const UseRequest = ()=>{
 	const [error, setError] = useState(null)
 
 	const request = useCallback((url, method = 'GET', headers={}, body=null)=>{
-		if
+		setLoading(true)
+		if(body){
+			body = JSON.stringify(body)
+		}
+		fetch(url,{method, headers, body})
+		.then(response =>{
+			if(response.ok){
+				return(response.json())
+			}
+			setError(true)
+			setLoading(false)
+		})
+		.then(responseData =>{
+			setLoading(false)
+			return(responseData)
+		})
+		.catch(new_error =>{
+			setError(true)
+			setLoading(false)
+			console.log(new_error)
+		})
+	return({request, loading, error})
 
 	},[])
 }
